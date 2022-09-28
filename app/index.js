8ab95dce2f6c43c61db22c37be303fdd0b98f2f6
Alizéa Massé <alizea777@hotmail.fr>
2022-09-28 13:51:03 +0200

    :wrench: config: add cors

--- app/index.js
+++ app/index.js
@@ -1,19 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-
-
 
 
 /** ********* */
 /*  EXPRESS */
 /** ******** */
-
-app.use(express.static('public'));
+const corsOptions = {credentials:true, origin: process.env.URL || '*'};
+const routerIndex = require("./router");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-const routerIndex = require("./router");
+app.use(cors(corsOptions));
 app.use(routerIndex);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 require("dotenv").config();
 
-
 const app = require("./app");
 
 
-
 const port = process.env.PORT;
-
 
 
 app.listen(port, () => {

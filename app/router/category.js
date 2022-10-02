81448a70a90b9b28bc763a8a706c1556714f1a75
Benoit-Castang <103994311+Benoit-Castang@users.noreply.github.com>
2022-10-02 12:42:06 +0200

    :construction: wip: Update creation of apiDoc

--- app/router/category.js
+++ app/router/category.js
@@ -8,6 +8,8 @@ router.get("/api/categories",categoryController.getAll);
 
 router.patch("/api/categories/:id",categoryController.edit)
 router.delete("/api/categories/:id",categoryController.delete)
+// route pour créer une category en admin (benoit)
+
 
 
 
